71d3b1f9850d4783ffb5d80062caedb8a4df1d0b
陆晨 <luchen0417@163.com>
2015-10-26 15:09:50 +0800

    后台模板

--- AdminWeb/App_Start/BundleConfig.cs
+++ AdminWeb/App_Start/BundleConfig.cs
@@ -22,10 +22,54 @@ namespace AdminWeb
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/assetsCss").Include(
+                    "~/assets/global/css/family.css",
+                    "~/assets/global/plugins/font-awesome/css/font-awesome.min.css",
+                    "~/assets/global/plugins/simple-line-icons/simple-line-icons.min.css",
+                    "~/assets/global/plugins/bootstrap/css/bootstrap.min.css",
+                    "~/assets/global/plugins/uniform/css/uniform.default.css",
+                    "~/assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css",
+                    "~/assets/global/plugins/bootstrap-daterangepicker/daterangepicker-bs3.css",
+                    "~/assets/global/plugins/fullcalendar/fullcalendar.min.css",
+                    "~/assets/global/plugins/jqvmap/jqvmap/jqvmap.css",
+                    "~/assets/admin/pages/css/tasks.css",
+                    "~/assets/global/css/components-rounded.css",
+                    "~/assets/global/css/plugins.css",
+                    "~/assets/admin/layout4/css/layout.css",
+                    "~/assets/admin/layout4/css/themes/light.css",
+                    "~/assets/admin/layout4/css/custom.css"
+                ));
+            bundles.Add(new ScriptBundle("~/bundles/assetsJS").Include(
+                "~/assets/global/plugins/jquery-migrate.min.js",
+                "~/assets/global/plugins/jquery-ui/jquery-ui.min.js",
+                "~/assets/global/plugins/bootstrap/js/bootstrap.min.js",
+                "~/assets/global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js",
+                "~/assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js",
+                "~/assets/global/plugins/jquery.blockui.min.js",
+                "~/assets/global/plugins/jquery.cokie.min.js",
+                "~/assets/global/plugins/uniform/jquery.uniform.min.js",
+                "~/assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js",
+                "~/assets/global/plugins/jqvmap/jqvmap/jquery.vmap.js",
+                "~/assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.russia.js",
+                "~/assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.world.js",
+                "~/assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.europe.js",
+                "~/assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.germany.js",
+                "~/assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.usa.js",
+                "~/assets/global/plugins/jqvmap/jqvmap/data/jquery.vmap.sampledata.js",
+                "~/assets/global/plugins/morris/morris.min.js",
+                "~/assets/global/plugins/morris/raphael-min.js",
+                "~/assets/global/plugins/jquery.sparkline.min.js",
+
+                "~/assets/global/scripts/metronic.js",
+                "~/assets/admin/layout4/scripts/layout.js",
+                "~/assets/admin/layout4/scripts/demo.js",
+                "~/assets/admin/pages/scripts/index3.js",
+                "~/assets/admin/pages/scripts/tasks.js"
+                ));
         }
     }
 }
