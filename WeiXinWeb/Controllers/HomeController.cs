1ab7bbf5dc6967c02ea2604e215c9ea228501489
陆晨 <luchen0417@163.com>
2015-11-28 17:54:29 +0800

    201511281754

---
+++ WeiXinWeb/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeiXinWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// 主页
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 馆内人数
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult PeopleNumber()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
